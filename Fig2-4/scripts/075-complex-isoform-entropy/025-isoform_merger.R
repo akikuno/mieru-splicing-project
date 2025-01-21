f2e4a662d4fe9faba17dcbe4840aa7ddf25d5867
akikuno <akikuno@users.noreply.github.com>
2025-01-21 15:14:20 +0900

    Update R script

--- Fig2-4/scripts/075-complex-isoform-entropy/025-isoform_merger.R
+++ Fig2-4/scripts/075-complex-isoform-entropy/025-isoform_merger.R
@@ -4,10 +4,10 @@ library(janitor)
 directory_path <- "data/rsem/bam"
 
 df_file_path <- list.files(directory_path, full.names = TRUE) %>%
-    as_tibble() %>%
+    as.data.frame() %>%
     set_names("file_path") %>%
     filter(str_detect(file_path, "isoforms.results")) %>%
-    filter(str_detect(file_path, "Cd2bp2|MIERU|Qk|Ubr5|Spen"))
+    filter(str_detect(file_path, "Cd2bp2|MIERU|Qk|Rbm24|Spen|Trim71|Ubr5|Wt1|Ybx1"))
 
 df_isoform <- tibble()
 for (file_path in df_file_path$file_path) {
@@ -20,11 +20,11 @@ for (file_path in df_file_path$file_path) {
     df_isoform <- bind_rows(df_isoform, file_data)
 }
 
-print(df_isoform)
+# print(df_isoform)
 
 dir.create("data/Fig7", showWarnings = FALSE)
 df_isoform %>%
     mutate(gene_symbol = str_remove(gene_id, "^.*_")) %>%
     mutate(transcript_symbol = str_remove(transcript_id, "^.*_")) %>%
     select(sample, gene_symbol, transcript_symbol, tpm) %>%
-    write_tsv("data/Fig7/tpm_isoforms_all.tsv")
+    write_tsv("data/Fig7/tpm_isoforms_all.tsv.gz")
--- Fig2-4/scripts/075-complex-isoform-entropy/035-isoform_diversity.R
+++ Fig2-4/scripts/075-complex-isoform-entropy/035-isoform_diversity.R
@@ -2,7 +2,7 @@ library(tidyverse)
 library(patchwork)
 library(ggsignif)
 
-df_isoforms <- read_tsv("data/Fig7/tpm_isoforms_all.tsv") %>%
+df_isoforms <- read_tsv("data/Fig7/tpm_isoforms_all.tsv.gz") %>%
     mutate(genes = toupper(gene_symbol)) %>%
     mutate(group = str_remove(sample, "_.*$"))
 
@@ -70,62 +70,83 @@ df_t_test <- tibble()
 input_ko_symbol <- ko_symbols[1]
 
 for (input_ko_symbol in ko_symbols) {
-    genes_se_complex <- df_complex %>%
+    genes_complex <- df_complex %>%
         filter(ko_symbol == input_ko_symbol) %>%
         select(genes)
     df_ko_isoforms <- df_isoforms %>%
         filter(str_detect(sample, input_ko_symbol)) %>%
-        inner_join(genes_se_complex, by = "genes", relationship = "many-to-many")
+        inner_join(genes_complex, by = "genes", relationship = "many-to-many")
     df_mieru_isoforms <- df_isoforms %>%
         filter(str_detect(sample, "MIERU")) %>%
-        inner_join(genes_se_complex, by = "genes", relationship = "many-to-many")
+        inner_join(genes_complex, by = "genes", relationship = "many-to-many")
 
     df_entropy <- tibble()
 
     df_ko_entropy <- df_ko_isoforms %>%
         select(sample, genes, tpm, group) %>%
-        group_by(sample, genes) %>%
+        group_by(group, genes) %>%
         mutate(entropy = calculate_entropy(tpm)) %>%
         ungroup() %>%
-        select(sample, group, entropy) %>%
+        select(group, genes, entropy) %>%
         distinct()
-
 
     df_mieru_entropy <- df_mieru_isoforms %>%
         select(sample, genes, tpm, group) %>%
-        group_by(sample, genes) %>%
+        group_by(group, genes) %>%
         mutate(entropy = calculate_entropy(tpm)) %>%
         ungroup() %>%
-        select(sample, group, entropy) %>%
+        select(group, genes, entropy) %>%
         distinct()
 
 
-    df_entropy <- bind_rows(df_ko_entropy, df_mieru_entropy)
+    df_entropy <- inner_join(df_ko_entropy, df_mieru_entropy, by = "genes", suffix = c("_ko", "_mieru"))
+    cat(input_ko_symbol)
+    print(t.test(df_entropy$entropy_ko, df_entropy$entropy_mieru, paired = TRUE)$p.value)
 
-    df_entropy <- df_entropy %>%
+    df_entropy_longer <-
+        df_entropy %>%
+        pivot_longer(
+            cols = starts_with("group"),
+            names_to = "key_group",
+            values_to = "group"
+        ) %>%
+        pivot_longer(
+            cols = starts_with("entropy"),
+            names_to = "key_entropy",
+            values_to = "entropy"
+        ) %>%
+        filter((key_group == "group_ko" & key_entropy == "entropy_ko") | (key_group == "group_mieru" & key_entropy == "entropy_mieru")) %>%
+        select(-key_group, -key_entropy)
+
+    df_entropy_longer <- df_entropy_longer %>%
         mutate(group = factor(group, levels = c("MIERU", input_ko_symbol)))
 
     fill_values <- c("MIERU" = "white") %>%
         c(setNames("#AAA", input_ko_symbol))
 
-    g_plot <- ggplot(df_entropy, aes(x = group, y = entropy, fill = group)) +
+    g_plot <- ggplot(df_entropy_longer, aes(x = group, y = entropy, fill = group)) +
         geom_violin() +
         geom_boxplot(width = 0.1, fill = "white") +
         geom_signif(
             comparisons = list(c("MIERU", input_ko_symbol)),
-            test = "t.test", na.rm = FALSE, map_signif_level = TRUE, col = "black", step_increase = 0.1
+            test = "t.test", test.args = list(paired = TRUE), na.rm = FALSE, map_signif_level = TRUE, col = "black", step_increase = 0.1
         ) +
         scale_fill_manual(values = fill_values) +
         labs(x = "Sample", y = "Isoform diversity (entropy)") +
         theme_bw()
 
     g_list[[input_ko_symbol]] <- g_plot
-    df_t_test <- bind_rows(df_t_test, tibble(ko_symbol = input_ko_symbol, p_value = t.test(df_ko_entropy$entropy, df_mieru_entropy$entropy)$p.value))
+    df_t_test <- bind_rows(df_t_test, tibble(
+        ko_symbol = input_ko_symbol,
+        p_value = t.test(df_ko_entropy$entropy, df_mieru_entropy$entropy, paired = TRUE)$p.value,
+        num_complex = nrow(genes_complex),
+        average_entropy_ko = mean(df_ko_entropy$entropy),
+        average_entropy_mieru = mean(df_mieru_entropy$entropy)
+    ))
 }
 
 g_wrap <- wrap_plots(g_list, ncol = 4)
 
-ggsave("reports/Fig7/isoform_diversity.png", g_wrap, width = 15, height = 5)
-# df_ko_isoforms <- df_isoforms %>%
-#     filter(!str_detect(sample, "MIERU")) %>%
-#     inner_join(genes_se_complex, by = "genes", relationship = "many-to-many")
+ggsave("reports/Fig7/isoform_diversity.png", g_wrap, width = 15, height = 10)
+df_t_test %>%
+    write_tsv("reports/Fig7/isoform_diversity_t_test.tsv")
