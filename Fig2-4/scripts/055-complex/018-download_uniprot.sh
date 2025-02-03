9d0253ed873a8a5f448af1afa8dfc7efe07784b0
akikuno <akikuno@users.noreply.github.com>
2025-02-03 10:46:54 +0900

    Update uniprot

--- Fig2-4/scripts/055-complex/018-download_uniprot.sh
+++ Fig2-4/scripts/055-complex/018-download_uniprot.sh
@@ -2,9 +2,20 @@
 
 mkdir -p data/Fig5
 
-time curl https://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/idmapping/idmapping.dat.gz > data/Fig5/uniprot_idmapping.txt.gz
+# time curl https://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/idmapping/idmapping.dat.gz > data/Fig5/uniprot_idmapping.txt.gz # 30min
+# Uniprot ID
+time curl https://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/idmapping/idmapping_selected.tab.gz > data/Fig5/uniprot_idmapping_selected.txt.gz
+time zcat data/Fig5/uniprot_idmapping_selected.txt.gz |
+    awk 'BEGIN{OFS="\t"}{print $13,$1,$2,$3}' |
+    sort > data/Fig5/uniprot_idmapping_selected_flatten.txt # 1m5.366s
 
-time awk -F'\t' '
+# NCBI Gene ID
+time curl https://ftp.ncbi.nlm.nih.gov/gene/DATA/GENE_INFO/Mammalia/All_Mammalia.gene_info.gz > data/Fig5/All_Mammalia.gene_info.gz # 4 min
+zcat data/Fig5/All_Mammalia.gene_info.gz | cut -f 1-3 | awk '$1 == 9606 || $1 == 10090' | sort > data/Fig5/gene_info_mouse_human.txt
+
+
+time zcat data/Fig5/uniprot_idmapping.txt.gz |
+    awk -F '\t' '
     $1 != prev {
         if (NR > 1) print line;
         prev = $1;
@@ -15,17 +26,21 @@ time awk -F'\t' '
         line = line "\t" $2 ":" $3;
     }
     END { print line; }
-    ' < data/Fig5/uniprot_idmapping.txt.gz |
-    gzip -c > data/Fig5/uniprot_idmapping_flatten.txt.gz
+    ' |
+    gzip -c > data/Fig5/uniprot_idmapping_flatten.txt.gz # 5m14.102s
 
 zcat data/Fig5/uniprot_idmapping_flatten.txt.gz | wc -l
-zcat data/Fig5/uniprot_idmapping_flatten.txt.gz |
+time zcat data/Fig5/uniprot_idmapping_flatten.txt.gz |
     grep Gene_Name |
     grep -e NCBI_TaxID:9606 -e NCBI_TaxID:10090 |
     awk '{print $1; for (i=1; i<=NF; i++) if ($i ~ /^Gene_Name:/ || $i ~ /^NCBI_TaxID/) print $i}' |
     paste - - - |
     sed -e "s|Gene_Name:||" -e "s|NCBI_TaxID:||" > data/Fig5/uniprot_idmapping_mouse_human.txt
 
+wc -l data/Fig5/uniprot_idmapping_mouse_human.txt
+ls -lh data/Fig5/uniprot_idmapping_mouse_human.txt
+
+zcat data/Fig5/uniprot_idmapping_flatten.txt.gz | grep -e Q9UK58 -e Q9UQ88 | tee tmp_uniprot.txt
 # curl https://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/idmapping/idmapping.dat.example |
 # awk -F'\t' '
 #     $1 != prev {
