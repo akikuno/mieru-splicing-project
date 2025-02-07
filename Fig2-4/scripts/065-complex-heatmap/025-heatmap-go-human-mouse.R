43ddee8ae4565c9c2768a0996e009ecdc61d30c6
akikuno <akikuno@users.noreply.github.com>
2025-02-07 13:47:28 +0900

    Add heatmap

--- Fig2-4/scripts/065-complex-heatmap/025-heatmap-go-human-mouse.R
+++ Fig2-4/scripts/065-complex-heatmap/025-heatmap-go-human-mouse.R
@@ -8,6 +8,7 @@
 library(tidyverse)
 library(org.Mm.eg.db)
 library(clusterProfiler)
+library(pheatmap)
 
 df_mgi_genes <- read_tsv("data/Fig5/mgi_protein_coding_symbols.txt")
 df_all <- read_csv("data/rmats/all_events_ko_target_fdr_dpsi.csv")
@@ -26,78 +27,90 @@ events <- df_all$event %>% unique()
 input_ko_symbol <- ko_symbols[1]
 input_event <- events[1]
 
-go_complex_genes <- df_complextab %>% count(go)
+if(!file.exists("data/Fig6/go_enrichments.csv")) {
+    go_enrichments <- tibble()
 
-genes_complex <- df_complextab$symbol %>% unique()
+    genes_complex <- df_complextab$symbol %>% unique()
+    for (input_ko_symbol in ko_symbols) {
+        spliced_genes <- df_spliced_genes %>%
+            filter(ko_symbol == input_ko_symbol) %>%
+            select(symbol = target_symbol) %>%
+            distinct()
+        spliced_genes_with_complex <- spliced_genes %>%
+            filter(symbol %in% genes_complex) %>%
+            pull(symbol) %>%
+            unique()
 
-go_enrichments <- tibble()
+        go_enrichment <- enrichGO(
+            gene = spliced_genes_with_complex,
+            universe = genes_complex,
+            OrgDb = org.Mm.eg.db,
+            keyType = "SYMBOL",
+            ont = "BP",
+            pAdjustMethod = "BH",
+            pvalueCutoff = 0.01,
+            qvalueCutoff = 0.05
+        )
+        go_enrichments <- as_tibble(go_enrichment) %>%
+            mutate(ko_symbol = input_ko_symbol) %>%
+            bind_rows(go_enrichments)
+    }
 
-for (input_ko_symbol in ko_symbols) {
-    spliced_genes <- df_spliced_genes %>%
-        filter(ko_symbol == input_ko_symbol) %>%
-        select(symbol = target_symbol) %>%
-        distinct()
-    spliced_genes_with_complex <- spliced_genes %>%
-        filter(symbol %in% genes_complex) %>%
-        pull(symbol) %>%
-        unique()
+    go_enrichments <- go_enrichments %>% janitor::clean_names()
 
-    go_enrichment <- enrichGO(
-        gene = spliced_genes_with_complex,
-        universe = genes_complex,
-        OrgDb = org.Mm.eg.db,
-        keyType = "SYMBOL",
-        ont = "BP",
-        pAdjustMethod = "BH",
-        pvalueCutoff = 0.01,
-        qvalueCutoff = 0.05
-    )
-    go_enrichments <- as_tibble(go_enrichment) %>%
-        mutate(ko_symbol = input_ko_symbol) %>%
-        bind_rows(go_enrichments)
+    write_csv(go_enrichments, "data/Fig6/go_enrichments.csv")
 }
-
-go_enrichments <- go_enrichments %>% janitor::clean_names()
-
-write_csv(go_enrichments, "data/Fig6/go_enrichments.csv")
-
-
-# go_enrichments %>% filter(ko_symbol == "Strap") %>%
-#     select(ko_symbol, ID, Description, FoldEnrichment, qvalue, geneID, GeneRatio)
 
 go_enrichments <- read_csv("data/Fig6/go_enrichments.csv")
 
 go_overlap <-
     go_enrichments %>%
     add_count(description, name = "go_count") %>%
-    filter(go_count > 8) %>%
+    filter(go_count > 9) %>%
     select(ko_symbol, description, fold_enrichment) %>%
+    complete(ko_symbol, description, fill = list(fold_enrichment = 0)) %>%
     distinct()
 
-go_overlap <- go_overlap %>% filter(description == "regulation of cell cycle")
-# gene_idの要素が90%以上マッチしているものは、同一のGOとして、qvalueがもっとも低いものを選択する
+mat_overlap <-
+    go_overlap %>%
+    pivot_wider(names_from = description, values_from = fold_enrichment) %>%
+    column_to_rownames("ko_symbol") %>%
+    as.matrix() %>%
+    t()
+
+dir.create("reports/Fig6", showWarnings = FALSE)
+pdf("reports/Fig6/heatmap_go_human_mouse.pdf", width = 15, height = 10)
+pheatmap(mat_overlap, scale = "none", color = colorRampPalette(c("white", "#fd7e00"))(10), fontsize = 20)
+dev.off()
+
+jpeg("reports/Fig6/heatmap_go_human_mouse.jpg", width = 15, height = 10, units = "in", res = 600)
+pheatmap(mat_overlap, scale = "none", color = colorRampPalette(c("white", "#fd7e00"))(10), fontsize = 20)
+dev.off()
+
+# go_overlap <- go_overlap %>% filter(description == "regulation of cell cycle")
+# # gene_idの要素が90%以上マッチしているものは、同一のGOとして、qvalueがもっとも低いものを選択する
 
 
-# gene_id をリストに変換
-go_overlap <- go_overlap %>%
-    mutate(gene_list = strsplit(gene_id, "/"))
+# # gene_id をリストに変換
+# go_overlap <- go_overlap %>%
+#     mutate(gene_list = strsplit(gene_id, "/"))
 
-# 類似度判定関数（90%以上の遺伝子が一致するか）
-similarity_check <- function(x, y) {
-    common_genes <- length(intersect(x, y))
-    min_length <- min(length(x), length(y))
-    return(common_genes / min_length)
-}
+# # 類似度判定関数（90%以上の遺伝子が一致するか）
+# similarity_check <- function(x, y) {
+#     common_genes <- length(intersect(x, y))
+#     min_length <- min(length(x), length(y))
+#     return(common_genes / min_length)
+# }
 
-# 全ペアの組み合わせを取得
-pairs <- expand.grid(idx1 = 1:nrow(go_overlap), idx2 = 1:nrow(go_overlap)) %>%
-    filter(idx1 < idx2)
+# # 全ペアの組み合わせを取得
+# pairs <- expand.grid(idx1 = 1:nrow(go_overlap), idx2 = 1:nrow(go_overlap)) %>%
+#     filter(idx1 < idx2)
 
-# 類似するペアをフィルタリング
-edges <-
-    pairs %>%
-    mutate(similar = pmap_dbl(list(idx1, idx2), function(i, j) {
-        similarity_check(go_overlap$gene_list[[i]], go_overlap$gene_list[[j]])
-    })) %>%
-    filter(similar > 0.75) %>%
-    select(idx1, idx2)
+# # 類似するペアをフィルタリング
+# edges <-
+#     pairs %>%
+#     mutate(similar = pmap_dbl(list(idx1, idx2), function(i, j) {
+#         similarity_check(go_overlap$gene_list[[i]], go_overlap$gene_list[[j]])
+#     })) %>%
+#     filter(similar > 0.75) %>%
+#     select(idx1, idx2)
